5f6878be6af676004aab28942f4d9ec6d09f40a5
Daniel Vazquez <da.vazquez728@gmail.com>
2022-01-19 15:19:48 -0700

    auth0 context provided to app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from "./state/store"
+import { Auth0Provider } from "@auth0/auth0-react";
 
-
+const authDomain = process.env.REACT_APP_AUTH0_DOMAIN;
+const authClient = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Auth0Provider
+    domain={authDomain}
+    clientId={authClient}
+    redirectUri={window.location.origin}
+    >
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  </Auth0Provider>,
   document.getElementById('root'))
