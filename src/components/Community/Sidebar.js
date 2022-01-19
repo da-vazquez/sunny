fc5a7cb42921a10e1a0c0efed6394ed039cf98e4
Daniel Vazquez <da.vazquez728@gmail.com>
2022-01-19 14:36:13 -0700

    sidebar component created to accompany community component

---
+++ src/components/Community/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar-container'>
+      <div className='sidebar-items'>
+        <h2>Moderators</h2>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
